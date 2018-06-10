8e221e9d8474200d7c6d05033ab18e34825cfd2d
grasshousedev <finnelyhawthorne@gmail.com>
2018-06-11 03:06:06 +0900

    update background.js

--- src/js/background.js
+++ src/js/background.js
@@ -1,11 +1,11 @@
 // Manual Test
 
-// chrome.storage.sync.clear();
+chrome.storage.sync.clear();
 var taskRepository = new TaskRepository(chrome.storage);
 var taskService = new TaskService(taskRepository);
-var response = taskService.addTask({title: "test", timeBlocks: 2, mode: "work"});
+taskService.addTask({title: "test1", timeBlocks: 2, mode: "work"});
+taskService.addTask({title: "test2", timeBlocks: 2, mode: "play"});
 
 chrome.storage.sync.get(["tasksMap"], function(result){
-    let task = result.tasksMap[response.task.id];
-    console.log(taskRepository.unserializeTask(task));
+    console.log(result);
 });
